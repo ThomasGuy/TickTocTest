81a8d24199cbd088fcd41e5b0263848a3d4e4f37
ThomasGuy <twguy66@gmail.com>
2018-07-07 10:32:50 +0100

    get rid of user table

--- tickToc_db.py
+++ tickToc_db.py
@@ -31,20 +31,6 @@ class MyMixin(object):
 		(self.__tablename__, self.MTS, self.Open, self.Close)
 
 
-class User(Base):
-	__tablename__ = 'users'
-	user_id = Column(Integer(), primary_key=True)
-	username = Column(String(15), nullable=False, unique=True)
-	email_address = Column(String(255), nullable=False)
-	password = Column(String(25), nullable=False)
-	created_on = Column(DateTime(), default=datetime.now)
-	updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
-
-	def __repr__(self):
-		return "User(username='{self.username}', " \
-		"email_address='{self.email_address}', ".format(self=self)
-
-
 class Avt(MyMixin, Base):
     pass
 
